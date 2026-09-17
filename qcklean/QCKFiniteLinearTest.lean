19c6db3e7318ce98b61c8908c7ec42d5b7cb7b99
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-18 11:31:38 +1200

    Focus FiniteLinear interface on closure slice

--- qcklean/QCKFiniteLinearTest.lean
+++ qcklean/QCKFiniteLinearTest.lean
@@ -8,10 +8,3 @@ open QCK
 #check QCK.closureIter_mono
 #check QCK.iSup_closureIter_eq_futureObservableSpan
 #check QCK.closureIter_stabilizes
-#check QCK.futureObservableSpan_dualCoannihilator
-#check QCK.futureObservableSpan_eq_dualAnnihilator
-#check QCK.finitePresentation
-#check QCK.finitePresentation_apply
-#check QCK.finitePresentation_ker
-#check QCK.finitePresentation_surjective
-#check QCK.canonicalEquivFinitePresentation
