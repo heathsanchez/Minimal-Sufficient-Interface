1799bc17db5a9a5b021d50f216324c336400b210
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-18 11:14:48 +1200

    Focus QCK FiniteLinear test on closure task

--- qcklean/QCKFiniteLinearTest.lean
+++ qcklean/QCKFiniteLinearTest.lean
@@ -3,22 +3,8 @@ import QCKFiniteLinear
 open QCK
 
 #check QCK.futureObservableSpan
-#check QCK.futureObservableSpan_dualCoannihilator
-#check QCK.futureObservableSpan_eq_dualAnnihilator
-#check QCK.finitePresentation
-#check QCK.finitePresentation_ker
-#check QCK.canonicalEquivFinitePresentation
-#check QCK.canonical_finrank_eq_futureObservableSpan
-#check QCK.sufficient_rank_ge_canonical
-#check QCK.operationDefect
-#check QCK.operationDefect_eq_zero_iff
-#check QCK.snapshotSafe
-#check QCK.snapshotReserveFinrank
-#check QCK.snapshotReserveMap
-#check QCK.maintainedSafe
-#check QCK.maintainedSafe_greatest
-#check QCK.snapshotReserve_le_maintainedReserve
-#check QCK.combined_first_component_independent
-#check QCK.reserveRank
-#check QCK.reserveRank_submodular
-#check QCK.interactingCapability_not_submodular
+#check QCK.closureStep
+#check QCK.closureIter
+#check QCK.closureIter_mono
+#check QCK.iSup_closureIter_eq_futureObservableSpan
+#check QCK.closureIter_stabilizes
