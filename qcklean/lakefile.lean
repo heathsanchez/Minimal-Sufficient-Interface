18a9ce89a1d7b9f5ff4c05b982f6d132c5d55983
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-18 07:21:38 +1200

    Add pinned QCK Mathlib project

---
+++ qcklean/lakefile.lean
@@ -0,0 +1,13 @@
+import Lake
+open Lake DSL
+
+package qck where
+  moreLeanArgs := #["-DautoImplicit=false"]
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @
+  "44ba35c6daa9d69aff8fed9fff9bbde17ded774d"
+
+@[default_target]
+lean_lib QCK where
+  srcDir := "."
