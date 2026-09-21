3dbd586e773c5d1f84d8ba4c1a1824a83ce64328
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-21 22:02:32 +1200

    test(nucleus): specify finite quiver interface

---
+++ qcklean/Nucleus/QuiverTest.lean
@@ -0,0 +1,27 @@
+import Nucleus.Quiver
+
+open CategoryTheory
+open Nucleus
+
+#check FiniteQuiver
+#check Vertex
+#check primitiveEdge
+
+inductive V | a | b deriving DecidableEq, Fintype, Repr
+inductive E | left | right deriving DecidableEq, Fintype, Repr
+
+def G : FiniteQuiver where
+  V := V
+  E := E
+  vFinite := inferInstance
+  eFinite := inferInstance
+  vDecEq := inferInstance
+  eDecEq := inferInstance
+  src := fun _ => V.a
+  tgt := fun _ => V.b
+
+example : primitiveEdge G E.left ≠ primitiveEdge G E.right := by
+  intro h
+  have hv : (primitiveEdge G E.left).val = (primitiveEdge G E.right).val :=
+    congrArg Subtype.val h
+  cases hv
